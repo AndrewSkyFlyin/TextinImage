0b47ef17c1e3a7307f6b992258f2a6ba7799101b
Andrew Nguyen <andrew.dd.n@gmail.com>
2017-04-06 13:05:06 -0700

    Added bit message limit checking and documentation.

--- main.py
+++ main.py
@@ -1,108 +1,131 @@
-import sys, argparse
+# NOTE: Some of the functions were created referencing Reza Nikoopour's version of this project.
+# url: https://github.com/rnikoopour/TextInImage
+
+import sys
+import argparse
 from PIL import Image
 
 
-def bit_string_to_8bits(bit_string):
-    preceding_zeroes = '0' * (8 - len(bit_string))
-    full_string = preceding_zeroes + bit_string
+# Expands a binary number to 8-bits if it isn't already.
+# Based on Reza Nikoopour's function
+def expand_to_8_bits(bit_string):
+    zeroes = '0' * (8 - len(bit_string))
+    full_string = zeroes + bit_string
     return full_string
 
 
-def bit_string_to_32bits(bit_string):
-    preceding_zeroes = '0' * (32 - len(bit_string))
-    full_string = preceding_zeroes + bit_string
+# Expands a binary number to 32-bits if it isn't already.
+# Based on Reza Nikoopour's function
+def expand_to_32_bits(bit_string):
+    zeroes = '0' * (32 - len(bit_string))
+    full_string = zeroes + bit_string
     return full_string
 
 
+# Converts a string of text into a binary string.
+# Based on Reza Nikoopour's function
 def string_to_bin(string):
     binary_string = ''
     for letter in string:
-        letter_as_int = ord(letter)
-        letter_as_binary = format(letter_as_int, 'b')
-        letter_as_binary = bit_string_to_8bits(letter_as_binary)
+        letter_as_int = ord(letter)  # Convert to ascii number
+        letter_as_binary = format(letter_as_int, 'b')   # Convert ascii number to binary
+        letter_as_binary = expand_to_8_bits(letter_as_binary)  # Expands binary number to 8 bits
         binary_string += letter_as_binary
     return binary_string
 
 
-#Converts a binary string into a normal text string.
+# Converts a binary string into a normal text string.
+# Based on Reza Nikoopour's function
 def bin_to_string(binary):
-    #Splits the binary into an array by chunks of 8 characters long.
+    # Splits the binary into an array by chunks of 8 characters long.
     binary_chunks = [binary[i:i+8] for i in range(0, len(binary), 8)]
     text = ''
     for chunk in binary_chunks:
-        #Converts each binary chunk into the ascii decimal value.
+        # Converts each binary chunk into the ascii decimal value.
         ascii_value = int(chunk, 2)
-        #Converts the ascii decimal value into its character representation.
-        #The char is then appended to the text string variable.
+        # Converts the ascii decimal value into its character representation.
+        # The char is then appended to the text string variable.
         text += chr(ascii_value)
     return text
 
 
-#Extracts the least significant bit value of the passed bit string.
-def extract_last_bit(byte):
-    #Convert the passed value into binary format.
-    last_bit = list(format(byte, 'b'))
-    #Return the last element of the last_bit array, which is the least significant bit.
+# Extracts the least significant bit value of the passed bit string.
+def extract_last_bit(integer):
+    # Convert the passed value into binary format.
+    last_bit = list(format(integer, 'b'))
+    # Return the last element of the last_bit array, which is the least significant bit.
     return last_bit[-1]
 
 
-#Calculates the number of bits required to embed/extract message.
+# Calculates the number of bits required to embed/extract message.
 def num_bits_in_string(string):
     return len(string) * 8
 
 
-#The main message extraction function.
-def extract_Image(image_data, number_of_bits, index):
+# The main message extraction function.
+def extract_image(image_data, number_of_bits, index):
     bit_string = ''
-    #cut_off is where tge message extraction will stop at.
+    # cut_off is where the message extraction will stop at.
     cut_off = index - number_of_bits
     while index >= cut_off:
-        #Extract the least significant bit of each RGB value and append to bit_string
+        # Extract the least significant bit of each RGB value and append to bit_string
+        # Referenced from Reza Nikoopour's bit extraction method.
         red, green, blue = image_data[index]
         index -= 1
         bit_string += extract_last_bit(red)
         bit_string += extract_last_bit(green)
         bit_string += extract_last_bit(blue)
-    #print(bit_string[:number_of_bits])
-    return bit_string[:number_of_bits]
+    # print(bit_string[:number_of_bits])  # Debug line
+    return bit_string[:number_of_bits]  # Returns everything in the bit_string up until the element equal to the
+                                         # number_of_bits variable.
 
 
+# Calculates the length of the message in number of bits.
 def extract_text_length(image_data, string_length_starting_pixel):
-    binary_length = extract_Image(image_data, binary_message_length, string_length_starting_pixel)
-    length2 = int(binary_length, 2)
+    binary_length = extract_image(image_data, binary_message_length, string_length_starting_pixel)
+    length2 = int(binary_length, 2)  # Binary to int conversion.
     return length2
 
 
-def embed_in_image(image_data, text, index):
-    num_bits_to_embed = num_bits_in_string(text)  #Calculates number of bits required to embed message.
+# The main function for embedding messages into images.
+def embed_in_image(image_data, text, index, max_pixel_capacity):
+    num_bits_to_embed = num_bits_in_string(text)  # Calculates number of bits required to embed message.
 
-    num_bits_to_embed = format(num_bits_to_embed, 'b')  #Convert to binary format.
-    num_bits_to_embed = bit_string_to_32bits(num_bits_to_embed)  #Expand binary number to 32 bits.
-    #print(num_bits_to_embed)
+    if num_bits_to_embed > max_pixel_capacity:
+        print('Inputted text to too large to embed.')
+        sys.exit(0)
 
-    text_in_binary = string_to_bin(text)   #Works
-    #print(text_in_binary)  #Debug line
+    num_bits_to_embed = format(num_bits_to_embed, 'b')  # Convert to binary format.
+    num_bits_to_embed = expand_to_32_bits(num_bits_to_embed)  # Expand binary number to 32 bits.
+    # print(num_bits_to_embed) #Debug line
 
+    text_in_binary = string_to_bin(text)   # Works
+    # print(text_in_binary)  #Debug line
+
+    # The binary message will be split into individual numbers and placed into an array sequentially.
     binary_string = []
-    binary_string += list(num_bits_to_embed)
-    binary_string.extend('0')   #This is to fill up the empty 33rd bit value and align everything.
-    binary_string += list(text_in_binary)
-    updated_image_data = image_data
+    binary_string += list(num_bits_to_embed)  # Appends num_bits_to_embed to array
+    binary_string.extend('0')   # Appends a filler '0' to the empty 33rd bit string, due to alignment reasons.
+    binary_string += list(text_in_binary)  # Appends the binary of the actually message to the array.
+    updated_image_data = image_data  # Creates a local version of image_data.
 
+    # This while loop will execute while binary_string is not empty.
+    # As this loop runs, certain lines will pop the first element of the array until the array empties.
+    # This looping termination method reference Reza Nikoopour's embed in binary function.
     while binary_string:
         red, green, blue = image_data[index]
         index -= 1
 
-        temp_var = list(format(red, 'b'));
-        temp_var[-1] = binary_string[0]
-        binary_string.pop(0)
-        temp_var = ''.join(temp_var)
+        temp_var = list(format(red, 'b'))     # Retrieve the bit string value of red into a temp_var array.
+        temp_var[-1] = binary_string[0]        # Replace the last element of temp_var with the new bit value.
+        binary_string.pop(0)                   # Pop the first element of the binary_string array.
+        temp_var = ''.join(temp_var)           # Joins all the elements of the temp_var array back into a single string.
         red = int(temp_var, 2)
         if not binary_string:
             updated_image_data[index+1] = (red, green, blue)
             break
 
-        temp_var = list(format(green, 'b'));
+        temp_var = list(format(green, 'b'))
         temp_var[-1] = binary_string[0]
         binary_string.pop(0)
         temp_var = ''.join(temp_var)
@@ -111,7 +134,7 @@ def embed_in_image(image_data, text, index):
             updated_image_data[index+1] = (red, green, blue)
             break
 
-        temp_var = list(format(blue, 'b'));
+        temp_var = list(format(blue, 'b'))
         temp_var[-1] = binary_string[0]
         binary_string.pop(0)
         temp_var = ''.join(temp_var)
@@ -128,26 +151,27 @@ def main(input_file, output_file, text, encrypt):
     image = Image.open(input_file)
     image_data = list(image.getdata())
     length, height = image.size
-    maxSize = (length * height) - 1
-    string_length_starting_pixel = maxSize
-    string_text_starting_pixel = maxSize - 11
+    max_size = (length * height) - 1
+    max_pixel_capacity = (max_size * 3) - 33
+    string_length_starting_pixel = max_size
+    string_text_starting_pixel = max_size - 11
 
     if encrypt:
-        updated_image = embed_in_image(image_data, text, maxSize)
+        updated_image = embed_in_image(image_data, text, max_size, max_pixel_capacity)
         image.putdata(updated_image)
         image.save(output_file, 'PNG')
     else:
         num_bits_to_extract = extract_text_length(image_data, string_length_starting_pixel)
-        #print(num_bits_to_extract) #
-        message = extract_Image(image_data, num_bits_to_extract, string_text_starting_pixel)
+        # print(num_bits_to_extract) #
+        message = extract_image(image_data, num_bits_to_extract, string_text_starting_pixel)
         message = bin_to_string(message)
         print(message)
 
-        #Debugging stuff
-        #print(maxSize)
-        #print("Number of arguments:", len(sys.argv))
-        #print("Arguments:", str(sys.argv))
-        #print(image_data[maxSize])
+        # Debugging stuff
+        # print(max_size)
+        # print("Number of arguments:", len(sys.argv))
+        # print("Arguments:", str(sys.argv))
+        # print(image_data[max_size])
 
 
 if __name__ == '__main__':
